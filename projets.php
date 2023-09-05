3c2a34a092595b3a583ac2d95fc0b410cc7e7205
Usine-Ecole <trainingcenter@usinecole.com>
2023-09-05 15:05:34 +0200

    Page de la liste des projets  de l'Usine-Ecole 4.0

---
+++ projets.php
@@ -0,0 +1,328 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4ERD0LRTX6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4ERD0LRTX6');
+</script>
+    <title>Usine-Ecole 4.0</title>
+    <meta content="L'Usine-Ecole 4.0 accompagne depuis son inauguration une trentaine d’étudiants en alternance au brevet de techniciens supérieur CPRP en partenariat avec le lycée Leonard-de-Vinci de Melun et près de 200 demandeurs d'emploi." name="description">
+    <meta content="Usine, école, usinage, mécanique, accompagnements, apprentissages, LMS" name="keywords">
+
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="./assets/images/favicon.ico" type="image/svg+xml">
+    <!-- Bundle CSS file -->
+    <link href="./assets/css/bundle.css" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- HEADER -->
+    <header id="header" class="header clearfix">
+        <div class="header-box">
+            <!-- Logo & Nav -->
+            <div class="clearfix logo-nav-box">
+                <div class="container">
+                    <div class="logo-nav-box-container">
+                        <!-- Logo -->
+                        <a href="#" class="logo d-flex align-items-center">
+                            <img src="./assets/images/logo-1.png" srcset="./assets/images/logo-1@2x.png 2x" class="logo-dark" alt="">
+                            <img src="./assets/images/logo-3.png" srcset="./assets/images/logo-3@2x.png 2x" class="logo-dark-2" alt="">
+                            <h1 class="d-none">Usine-Ecole 4.0</h1>
+                        </a>
+                        <!-- Logo -->
+                        <!-- Nav menu -->
+                        <nav id="navbar" class="navbar">
+                            <ul>
+
+
+                                <li class="dropdown has-icon">
+                                    <a href="logout_projet" class="k-hover">
+                                        <span>Déconnexion</span>
+                                    </a>
+                                </li>
+
+                            </ul>
+                        </nav>
+                        <!-- Nav menu -->
+                        <!-- Mobile toggle -->
+                        <i class="mobile-nav-toggle mobile-nav-show ficon ficon-hamburger-menu"></i>
+                        <i class="mobile-nav-toggle mobile-nav-hide d-none ficon ficon-x-icon"></i>
+                        <!-- Mobile toggle -->
+                    </div>
+                </div>
+            </div>
+            <!-- Logo & Nav -->
+        </div>
+    </header>
+    <!-- HEADER -->
+    <main id="main">
+        <section id="tours" class="clearfix tours-box  bg-transparent-top pd-top-600 pd-bottom-1000">
+            <!-- Title, Breadcrumb & Description -->
+            <div class="title-box mg-bottom-400">
+                <div class="container">
+                    <div class="pd-right-30pc mb-5" data-aos-delay="300" data-aos="fade-in">
+                        <div class="clearfix">
+                            <div class="heading heading-sub">
+                                <span class="sub-title line-before" style="color: white;">Usine-Ecole 4.0</span>
+                                <h1 class="title title-lg fw-light" style="color: white;">Nos Projets</h1>
+                            </div>
+                            <div class="breadcrumb-box">
+                                <nav aria-label="breadcrumb">
+                                    <ol class="breadcrumb">
+                                        <li class="breadcrumb-item"><a href="index" class="">Accueil</a></li>
+                                        <li class="breadcrumb-item"><a href="#" class="">Nous connaitre</a></li>
+                                        <li class="breadcrumb-item active" aria-current="page" style="color: white;">Nos projets</li>
+                                    </ol>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Title, Breadcrumb & Description -->
+            <!-- Tour list -->
+            <br><br>
+            <div class="tour-list">
+                <div class="container">
+                    <div class="tour-list-container">
+                        <div class="row grid-tour-2col g-0 g-lg-5">
+                            <div class="col-12 col-xl-4 col-lg-6 grid-sizer"></div>
+                            <div class="col-12 col-xl-4 col-lg-6 grid-item mb-5 mb-lg-0">
+                                <!-- Tour item -->
+                                <a href="single-tour.html" class="clearfix tour-item" data-aos-delay="300" data-aos="fade-in">
+                                    <div class="tour-item-container">
+                                        <div class="tour-item-content">
+                                            <!-- Image -->
+                                            <figure class="image">
+                                                <img src="assets/img/tours/t1.jpg" srcset="assets/img/tours/t1@2x.jpg 2x" class="img-fluid" alt="">
+                                            </figure>
+                                            <!-- Image -->
+                                            <!-- Content -->
+                                            <div class="content">
+                                                <div class="heading heading-sub">
+                                                    <span class="sub-title line-before line-before-50">IPREXYS</span>
+                                    
+                                                </div>
+                                                <div class="description">
+                                                    <p>Projet de fabrication de pièces pour un prototype en R&D. </p>
+                                                
+                                                </div>
+                                            </div>
+                                            <!-- Content -->
+                                        </div>
+                                    </div>
+                                </a>
+                                <!-- Tour item -->
+                            </div>
+                            <div class="col-12 col-xl-4 col-lg-6 grid-item mb-5 mb-lg-0">
+                                <!-- Tour item -->
+                                <a href="single-tour.html" class="clearfix tour-item" data-aos-delay="300" data-aos="fade-in">
+                                    <div class="tour-item-container">
+                                        <div class="tour-item-content">
+                                            <!-- Image -->
+                                            <figure class="image">
+                                                <img src="assets/img/tours/t2.jpg" srcset="assets/img/tours/t2@2x.jpg 2x" class="img-fluid" alt="">
+                                            </figure>
+                                            <!-- Image -->
+                                            <!-- Content -->
+                                            <div class="content">
+                                                <div class="heading heading-sub">
+                                                    <span class="sub-title line-before line-before-50">Nico</span>
+                                                  
+                                                </div>
+                                                <div class="description">
+                                                    <p>Projet de conception d'outils spécifiques pour prothèses bioniques. </p>
+                                                    
+                                                </div>
+                                            </div>
+                                            <!-- Content -->
+                                        </div>
+                                    </div>
+                                </a>
+                                <!-- Tour item -->
+                            </div>
+                            <div class="col-12 col-xl-4 col-lg-6 grid-item mb-5 mb-lg-0">
+                                <!-- Tour item -->
+                                <a href="single-tour.html" class="clearfix tour-item" data-aos-delay="300" data-aos="fade-in">
+                                    <div class="tour-item-container">
+                                        <div class="tour-item-content">
+                                            <!-- Image -->
+                                            <figure class="image">
+                                                <img src="assets/img/tours/t4.jpg" srcset="assets/img/tours/t4@2x.jpg 2x" class="img-fluid" alt="">
+                                            </figure>
+                                            <!-- Image -->
+                                            <!-- Content -->
+                                            <div class="content">
+                                                <div class="heading heading-sub">
+                                                    <span class="sub-title line-before line-before-50">Heito </span>
+                                                  
+                                                </div>
+                                                <div class="description">
+                                                    <p>R&D en automatisation de machines spécifiques.</p>
+                                                   
+                                                </div>
+                                            </div>
+                                            <!-- Content -->
+                                        </div>
+                                    </div>
+                                </a>
+                                <!-- Tour item -->
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Tour list -->
+
+        </section>
+    </main>
+
+    <!-- FOOTER -->
+    <footer id="footer" class="footer">
+        <div class="container">
+            <!-- Block -->
+            <!-- Footer top -->
+            <div class="footer-top">
+                <div class="row">
+                    <div class="col-12 col-xl-3 col-lg-12 col-md-6">
+                        <!-- Brand -->
+                        <div class="footer-widget">
+                            <h4 class="widget-title">Usine-Ecole 4.0</h4>
+                            <p>
+                                Pôle de ressources et d'accompagnement vers les métiers mécaniques aéronautiques et industriels soutenu par l'Europe.
+                            </p>
+                        </div>
+                        <!-- Brand -->
+                    </div>
+                    <div class="col-12 col-xl-3 col-lg-4 col-md-6">
+                        <!-- Contact Info -->
+                        <div class="footer-widget">
+                            <h4 class="widget-title">Informations</h4>
+                            <div class="contact-box">
+                                <p>
+                                    Aérodrome de Melun Villaroche <br> 77950 Montereau-sur-le-Jard
+                                </p>
+                                <p>
+                                    <span> <a href="tel:+330160681144"></a> 01 60 68 11 44</span>
+                                </p>
+                                <p>
+                                    <a href="mailto:trainingcenter@usinecole.com" class="k-hover">trainingcenter@usinecole.com
+                                    </a>
+                                </p>
+                            </div>
+                        </div>
+                        <!-- Contact Info -->
+                    </div>
+                    <div class="col-12 col-xl-3 col-lg-4 col-md-6">
+                        <!-- Liens rapides -->
+                        <div class="footer-widget">
+                            <h4 class="widget-title">Liens rapides</h4>
+                            <div class="row g-3">
+                                <div class="col-6">
+                                    <ul class="list-unstyled footer-list">
+                                        <li>
+                                            <a href="/" class="k-hover">Accueil</a>
+                                        </li>
+                                        <li>
+                                            <a href="partenaires" class="k-hover">Partenaires</a>
+                                        </li>
+                                        <li>
+                                            <a href="soutenir" class="k-hover">Nous soutenir</a>
+                                        </li>
+                                        
+                                        <li>
+                                            <a href="apprenti" class="k-hover">Accompagnement</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="col-6">
+                                    <ul class="list-unstyled footer-list">
+                                        <li>
+                                            <a href="actu" class="k-hover">Actualités</a>
+                                        </li>
+                                        <li>
+                                            <a href="https://www.paris-villaroche-training-center.parcours.pro/" class="k-hover" target="_blank">Plateforme</a>
+                                        </li>
+                                        <li>
+                                            <a href="register_soft" class="k-hover">Soft-Skills</a>
+                                        </li>
+                                        <li>
+                                            <a href="contact" class="k-hover">Contact</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Liens rapides -->
+                    </div>
+                    <div class="col-12 col-xl-3 col-lg-4 col-md-6">
+                        <!-- Subscribe & Social -->
+                        <div class="footer-widget">
+                            <!-- Subscribe -->
+                            <div class="footer-social">
+                                <!-- Social -->
+                                <h4 class="social-title">Réseaux-Sociaux</h4>
+                                <div class="social-list">
+                                    <a href="https://m.facebook.com/trainingcentersympav/" class="btn-social btn-facebook k-hover"><i class="pe-so-facebook"></i></a>
+                                    <a href="https://twitter.com/TCenter4_0/" class="btn-social btn-twitter k-hover"><i class="pe-so-twitter"></i></a>
+                                    <a href="https://www.instagram.com/trainingcenter4.0/" class="btn-social btn-youtube-1 k-hover"><i class="pe-so-youtube-1"></i></a>
+                                    <a href="https://www.youtube.com/channel/UCqan073e4fkLA_HtG_uWk8Q" class="btn-social btn-vimeo k-hover"><i class="pe-so-instagram"></i></a>
+                                    <a href="https://www.linkedin.com/in/tcenter-sympav-28bb571a3/" class="btn-social btn-linkedin k-hover"><i class="pe-so-linkedin"></i></a>
+                                </div>
+                                <!-- Social -->
+                            </div>
+                        </div>
+                        <!-- Subscribe & Social -->
+                    </div>
+                </div>
+            </div>
+            <!-- Footer top -->
+            <!-- Footer Bottom -->
+            <div class="footer-bottom">
+                <div class="row">
+                    <div class="col-12 col-md-6">
+                        <!-- Copyright -->
+                        <p>© 2023 Usine-Ecole 4.0 Tous droits réservés.</p>
+                        <!-- Copyright -->
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <!-- Bottom links -->
+                        <div class="bottom-link text-start text-md-end">
+                            <ul class="list-inline">
+                                <li class="list-inline-item me-4">
+                                    <a href="mentions" target="_blank">Mentions légales</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="politique" target="_blank">Politique de confidentialité</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- Bottom links -->
+                    </div>
+                </div>
+            </div>
+            <!-- Footer Bottom -->
+            <!-- Block -->
+        </div>
+    </footer>
+    <!-- FOOTER -->
+
+    <a href="#" class="scroll-top d-flex align-items-center justify-content-center k-hover"><i class="ficon ficon-thin-arrow-up"></i></a>
+
+    <div id="preloader"></div>
+    <!-- Bundle JS Files -->
+    <script src="./assets/js/script.js"></script>
+    <script src="./assets/js/bundle.js"></script>
+</body>
+
+</html>
