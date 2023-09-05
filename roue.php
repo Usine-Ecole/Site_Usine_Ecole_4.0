b91f25c814268de7a83f1a6f408722e8be26e4c2
Usine-Ecole <trainingcenter@usinecole.com>
2023-09-05 15:12:53 +0200

    Message d'affichage de validation des soft-skills envoyé par mail

---
+++ roue.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4ERD0LRTX6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4ERD0LRTX6');
+</script>
+   <meta charset="utf-8">
+   <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+   <!-- Favicons -->
+   <link rel="shortcut icon" href="./assets/images/favicon.ico" type="image/svg+xml">
+   <!-- Bundle CSS file -->
+   <link href="./assets/css/bundle.css" rel="stylesheet" />
+   <title>Roue</title>
+</head>
+
+<body>
+   <section class="clearfix login-box fade-in bg-light pd-top-600 pd-bottom-600" style="height: 100vh;">
+      <div class="login">
+         <div class="container">
+            <div class="row justify-content-center">
+               <div class="col-12 col-xl-8 col-lg-6 col-lg-7" data-aos-delay="300" data-aos="fade-up">
+                  <div class="card">
+                     <div class="card-body p-4">
+                        <h1 style="text-align: center;">Merci, l'enregistrement des éléments a été effectué avec succès !</h1>
+                        <br>
+                        <p style="text-align: center;">Vos résultats vous seront envoyés par mail.</p>
+                        <p style="text-align: center;">Pour toute information complémentaire, n'hésitez pas à nous contacter par mail: trainingcenter@parisvillaroche.com</p>
+                        <br>
+                        <div class="d-flex justify-content-center">
+                           <a href="user_page" class="btn btn-lg btn-ht-primary btn-send mb-3 k-hover">Retour</a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+
+   </section>
+
+   <div id="preloader"></div>
+
+   <!-- Bundle JS Files -->
+   <script src="./assets/js/bundle.js"></script>
+</body>
+
+</html>
